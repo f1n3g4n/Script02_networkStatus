e98c323e6f7b9927302d40279f947fb480565723
F1neg4n <126989239+F1neg4n@users.noreply.github.com>
2023-03-13 12:39:23 -0300

    Update networkStatus.py

--- networkStatus.py
+++ networkStatus.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
+#Script by F1neg4n
 
 import nmap
 import os
 
 def welcome():
-    welc = 'NMAP Network Status by F1neg4n'
+    welc = 'NMAP Network Status'
+    info = '[INFO] Python script to scan ports with NMAP'
     os.system('clear')
     print(welc + '\n' + '*' * len(welc))
+    print(info)
+    print('------------')
     return
 
 def networkStatus():
     welcome()
-    print('[+] Scanning network...\n')
+    print('[+] Scanning network...')
     nm = nmap.PortScanner()
     nm.scan(hosts='192.168.1.0/24', arguments='-n -sP -PE -PA21,23,80,3389')
     hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+    print('------------')
     print('Host\t\t   Status')
     print('----\t\t   ------')
     for host, status in hosts_list:
